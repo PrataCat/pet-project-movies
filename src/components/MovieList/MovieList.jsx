0c55cf3e5092ea0e7864126368514f437885f625
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-10-31 21:48:49 +0200

    make a MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,25 @@
-const MovieList = () => {
-  return <h1>This is a MovieList</h1>;
+import MovieCard from '../MovieCard';
+
+const MovieList = ({ movies, pageTitle }) => {
+  return (
+    <>
+      <h1>{pageTitle}</h1>
+      <ul>
+        {movies.map(movie => {
+          const { id, poster_path, title, release_date } = movie;
+          return (
+            <MovieCard
+              id={id}
+              key={id}
+              poster={poster_path}
+              title={title}
+              release={release_date}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default MovieList;
