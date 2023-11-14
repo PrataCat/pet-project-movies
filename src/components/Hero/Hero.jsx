8e424cd118a2123c39984a4ea0f6ce193d77c98c
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-11-14 16:22:16 +0200

    finish Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,10 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className="hero-wrap">
-      <div className="hero-container"></div>
+      <div className="hero-overlay"></div>
+      <h1 className="hero-title">
+        Millions of movies, series and characters <br /> Explore now
+      </h1>
     </div>
   );
 };
